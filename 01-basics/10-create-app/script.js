235b4034c5e7ec36b5e29e1bc02f0dbed55169e3
Liliya Kulishova <lilyusha-kul@yandex.ru>
2024-10-02 21:40:05 +0300

    first task

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,8 @@
 import { defineComponent, createApp } from 'vue'
+const App = defineComponent({
+    name: 'App',
+    template:`<div>Сегодня {{ new Intl.DateTimeFormat("en", {dateStyle: "long"}).format(now) }}</div>`,
+    
+}) 
+
+createApp(App).mount('#app')
--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -1,47 +1,53 @@
 import { defineComponent } from 'vue'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
+
 export default defineComponent({
   name: 'WeatherApp',
-
+  
+  setup() {
+    return { 
+      cities: getWeatherData(),
+      WeatherConditionIcons,
+     }
+  },
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
-
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
+      <li v-for="value in cities" :class= "['weather-card', {'weather-card--night':(value.current.sunrise>value.current.dt && value.current.dt<value.current.sunset)}]">     
+      <div v-if="value.alert" class="weather-alert">
             <span class="weather-alert__icon">⚠️</span>
             <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{ value.geographic_name }}
             </h2>
             <div class="weather-card__time">
-              07:17
+            {{ value.current.dt }}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
+          <div class="weather-conditions__icon" :title="value.current.weather.description">{{WeatherConditionIcons[value.current.weather.id]}}</div>  
+            <div class="weather-conditions__temp">{{ (value.current.temp - 273.15).toFixed(1)}} °C</div>
           </div>
           <div class="weather-details">
             <div class="weather-details__item">
               <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
+              <div class="weather-details__item-value">{{ (value.current.pressure * 0.75).toFixed(0)}}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value"> {{ value.current.humidity }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">{{ value.current.clouds }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">{{ value.current.wind_speed }}</div>
             </div>
           </div>
         </li>
@@ -49,3 +55,4 @@ export default defineComponent({
     </div>
   `,
 })
+
